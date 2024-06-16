b47123ca7cbe9c9b08e79564bbf7270ea005cbd0
Jaeveous Hardy <hardyj3749@student.faytechcc.edu>
2024-06-16 02:33:16 +0000

    Exponents and Subtraction

---
+++ P1HW1_HardyJaeveous.py
@@ -0,0 +1,30 @@
+#Jaeveous Hardy
+#June 15, 2024
+#P1HW1
+#Practice with user being able to input intergers 
+
+print('-----Calculating Exponents----')
+print()
+print()
+print('Enter an integer as the base value:', end=' ')
+base = int(input())
+print('Enter an integer as the exponent:', end=' ')
+exponent = int(input())
+print(base, 'raised to the power of', exponent, 'is',base**exponent, '!!')
+print()
+print()
+print('-----Addition and Subtraction----')
+print()
+print()
+print('Enter a starting integer:', end=' ')
+starting = int(input())
+print('Enter an integer to add:', end=' ')
+add = int(input())
+print('Enter and integer to subtract:', end=' ')
+subtract = int(input())
+print()
+print()
+print(starting, '+', add, '-', subtract, 'is equal to', starting+add-subtract)
+
+
+
