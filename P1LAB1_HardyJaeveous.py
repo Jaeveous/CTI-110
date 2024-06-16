0382903ff264dc574698b29f0fe6fc5afda7c128
Jaeveous Hardy <hardyj3749@student.faytechcc.edu>
2024-06-15 21:15:16 -0400

    Add files via upload
    First submission.

---
+++ P1LAB1_HardyJaeveous.py
@@ -0,0 +1,13 @@
+#Jaeveous Hardy
+#June 15, 2024
+#P1LAB1
+#Writing my first code with user input, excciting!
+
+print('Enter your first name:', end=' ')
+first_name = input()
+
+print('Enter your last name:', end=' ')
+last_name = input()
+
+print('Hello,', first_name, last_name,'! Welcome to CTI-110')
+
