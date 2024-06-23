eb4da5fee97d4b128d033aefb30484599fcec370
Jaeveous Hardy <hardyj3749@student.faytechcc.edu>
2024-06-23 19:02:16 -0400

    Are of a Circle Calculations
    This program will calculate the diameter, circumference, and area of a circle once the user inputs the radius.

---
+++ P2LAB1_HardyJaeveous.py
@@ -0,0 +1,18 @@
+#Jaeveous Hardy
+#June 23, 2024
+#P2LAB1
+#Using Math Expressions
+import math
+
+#Get the radius
+radius = float(input("Enter the radius: "))
+
+#Calculate circum, diameter, and area
+cir = 2 * math.pi * radius
+diam = 2 * radius
+area = math.pi * (radius ** 2)
+
+#Display results
+print(f"The diameter of the circle is {diam: .1f}")
+print(f"The circumference of the circle is {cir: .2f}")
+print(f"The area of the circle is {area: .3f}")
