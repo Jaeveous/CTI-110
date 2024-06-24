d2d353da9cb8a728f208de923d4c3ab198883b11
Jaeveous Hardy <hardyj3749@student.faytechcc.edu>
2024-06-23 22:04:55 -0400

    Travel Expense Calculator 2.0
    Upgraded results to have floating strings with two decimal places

---
+++ P2HW1_HardyJaeveous.py
@@ -0,0 +1,27 @@
+#Jaeveous Hardy
+#June 23, 2024
+#P2HW1
+#Calculating and displaying travel expenses
+print('This program calculates and displays travel expenses')
+print()
+print('Enter Budget:', end=' ')
+budget = float(input())
+print('Enter your travel destination:', end=' ')
+destination = input()
+print('How much do you think you will spend on gas?', end=' ')
+gas = float(input())
+print('Approximately, how much will need for accomodation/hotel?', end=' ')
+hotel = float(input())
+print('Last, how much do you need for food?', end=' ')
+food = float(input())
+print()
+print('------------Travel expenses------------')
+print('Location:', destination)
+print(f'Initial Budget: ${budget: .2f}')
+print(f'Fuel: ${gas: .2f}')
+print(f'Accomodation: ${hotel: .2f}')
+print(f'Food: ${food: .2f}')
+print('---------------------------------------')
+print()
+print()
+print(f'Remaining Balance: ${budget-gas-hotel-food: .2f}')
