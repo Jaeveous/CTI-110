fa495f65a39e77eb6a0c9ac0fa94f95887a29fdb
Jaeveous Hardy <hardyj3749@student.faytechcc.edu>
2024-07-07 17:32:02 -0400

    Multiplication chart from [1,12]
    This program allows a user to enter an integer for it to be multiplied from 1-12.

---
+++ P4LAB2_HardyJaeveous.py
@@ -0,0 +1,53 @@
+#Jaeveous Hardy
+#July 7, 2024
+#P4LAB2
+#Multiplication Chart
+
+count = int(input('Enter an interger: '))
+
+if count < 0:
+    print('Can not accept negative values')
+    
+
+
+else:
+    for i in [1,2,3,4,5,6,7,8,9,10,11,12]:
+        print(count,"*", i, "=", count * i)
+
+
+
+user = input('Would you like to run the program again?')
+
+if user == 'no':
+    print('Have a good day!')
+
+else:
+    count = int(input('Enter an interger: '))
+
+    if count < 0:
+            print('Can not accept negative values')
+    
+    else:
+        for i in [1,2,3,4,5,6,7,8,9,10,11,12]:
+            print(count,"*", i, "=", count * i)
+
+jay = 0
+
+while jay < 3:
+    print ('Have good day!')
+    jay = jay + 1
+    
+    
+    
+
+
+
+  
+
+
+
+
+       
+
+
+        
